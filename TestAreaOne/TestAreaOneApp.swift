90ede9a27791975e350f21d6c3ef169ad21ffee0
Sven Svensson <sven@bringsrud.net>
2022-05-06 17:01:56 +0200

    Initial Commit

---
+++ TestAreaOne/TestAreaOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAreaOneApp.swift
+//  TestAreaOne
+//
+//  Created by Sven Svensson on 06/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestAreaOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
