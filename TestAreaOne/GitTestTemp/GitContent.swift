d5901250e4746e1a56b72d79b374f6ff0677c87d
Sven Svensson <sven@bringsrud.net>
2022-05-18 11:56:00 +0200

    squashed and reset head^

---
+++ TestAreaOne/GitTestTemp/GitContent.swift
@@ -0,0 +1,23 @@
+//
+//  GitContent.swift
+//  TestAreaOne
+//
+//  Created by Sven Svensson on 18/05/2022.
+//
+
+import SwiftUI
+
+struct GitContent: View {
+    var body: some View {
+        VStack {
+        Text("Hello, World!, testing git reset HEAD^, what does it mean")
+            .padding()
+        Text("new")
+    }
+}
+
+struct GitContent_Previews: PreviewProvider {
+    static var previews: some View {
+        GitContent()
+    }
+}
