a2814934b0b02779654fdba9cfa5dda613450757
Sven Svensson <sven@bringsrud.net>
2022-05-18 12:50:14 +0200

    Create AView.swift

---
+++ TestAreaOne/GitTestTemp/AView.swift
@@ -0,0 +1,20 @@
+//
+//  AView.swift
+//  TestAreaOne
+//
+//  Created by Sven Svensson on 18/05/2022.
+//
+
+import SwiftUI
+
+struct AView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AView_Previews: PreviewProvider {
+    static var previews: some View {
+        AView()
+    }
+}
