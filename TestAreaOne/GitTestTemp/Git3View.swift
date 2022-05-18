29953151c0ff7836cd6cad7d7f538f6e6577e467
Sven Svensson <sven@bringsrud.net>
2022-05-18 12:11:38 +0200

    Create Git3View.swift

---
+++ TestAreaOne/GitTestTemp/Git3View.swift
@@ -0,0 +1,20 @@
+//
+//  Git3View.swift
+//  TestAreaOne
+//
+//  Created by Sven Svensson on 18/05/2022.
+//
+
+import SwiftUI
+
+struct Git3View: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct Git3View_Previews: PreviewProvider {
+    static var previews: some View {
+        Git3View()
+    }
+}
